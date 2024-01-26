004bd3d2da0ddaeda596d876f88e984e24d30d62
KimiaMontazeri <kimia.montazerii@snapp.cab>
2024-01-26 20:20:34 +0330

    feat: implement login page

---
+++ front/src/pages/login/login.tsx
@@ -0,0 +1,89 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Stack,
+  Text,
+  useToast,
+} from '@chakra-ui/react';
+
+function Login() {
+  const navigate = useNavigate();
+  const toast = useToast();
+
+  const [username, setUsername] = useState('');
+  const [isUsernameInvalid, setIsUsernameInvalid] = useState(false);
+
+  // password
+  const [password, setPassword] = useState('');
+  const [isPasswordValid, setIsPasswordValid] = useState(false);
+
+  const handleClickOnSignup = () => {
+    navigate('/signup');
+  };
+
+  const handleSubmit = (e: SubmitEvent) => {
+    e.preventDefault();
+
+    setIsUsernameInvalid(!username);
+    setIsPasswordValid(!password);
+
+    const shouldReturn = !username || !password;
+    if (shouldReturn) {
+      return;
+    }
+
+    // TODO: call /login
+    // if ok
+    toast({
+      title: 'Successfully logged in.',
+      status: 'success',
+      duration: 2000,
+      isClosable: true,
+    });
+  };
+
+  return (
+    <Stack align='center' width='450px'>
+      <Heading as='h1' size='xl'>
+        Hi, Welcome Back! 👋
+      </Heading>
+      <Text pb={4}>Hello again, you've been missed!</Text>
+      <form>
+        <Stack width='300px'>
+          <FormControl isRequired>
+            <FormLabel>Username</FormLabel>
+            <Input
+              value={username}
+              isInvalid={isUsernameInvalid}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder='Enter your username'
+            />
+          </FormControl>
+          <FormControl isRequired>
+            <FormLabel>Password</FormLabel>
+            <Input
+              value={password}
+              isInvalid={isPasswordValid}
+              onChange={(e) => setPassword(e.target.value)}
+              type='password'
+              placeholder='Enter your password'
+            />
+          </FormControl>
+          <Button type='submit' onClick={handleSubmit}>
+            Continue
+          </Button>
+          <Button variant='outline' onClick={handleClickOnSignup}>
+            Don't have an account?
+          </Button>
+        </Stack>
+      </form>
+    </Stack>
+  );
+}
+
+export default Login;
--- front/src/router/router.tsx
+++ front/src/router/router.tsx
@@ -1,5 +1,6 @@
 import { Outlet, createBrowserRouter } from 'react-router-dom';
 import Signup from '../pages/signup';
+import Login from '../pages/login';
 import Home from '../pages/home';
 import { TokenGuard } from './guards/token-guard';
 
@@ -24,10 +25,10 @@ export const router = createBrowserRouter([
     ],
   },
   // unauthorized paths
-  // {
-  //   path: '/login',
-  //   element: <Login />,
-  // },
+  {
+    path: '/login',
+    element: <Login />,
+  },
   {
     path: '/signup',
     element: <Signup />,
