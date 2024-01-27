077a1096ef675a5d0ac774ae53da6e4f6b7a600f
KimiaMontazeri <kimia.montazerii@snapp.cab>
2024-01-28 00:54:06 +0330

    feat: add home page

--- front/src/pages/home/home.tsx
+++ front/src/pages/home/home.tsx
@@ -1,13 +1,32 @@
-import { Button, Stack, useColorMode } from '@chakra-ui/react';
+import { Divider, Stack } from '@chakra-ui/react';
+import ChatList from '../../components/chat-list';
+import ChatBox from '../../components/chat-box';
+import ChatDetails from '../../components/chat-details';
+import SideBar from '../../components/side-bar';
 
 function Home() {
-  const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <Stack>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
-      <Button onClick={toggleColorMode}>
-        Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
-      </Button>
+    <Stack
+      flexDirection='row'
+      justifyContent='space-evenly'
+      gap={0}
+      alignItems='stretch'
+      height='100vh'
+    >
+      <Stack flexGrow={0.25}>
+        <SideBar />
+      </Stack>
+      <Divider orientation='vertical' />
+      <Stack flexGrow={1}>
+        <ChatList />
+      </Stack>
+      <Divider orientation='vertical' />
+      <Stack flexGrow={2}>
+        <ChatBox />
+      </Stack>
+      <Stack bgColor='papayawhip' flexGrow={1}>
+        <ChatDetails />
+      </Stack>
     </Stack>
   );
 }
