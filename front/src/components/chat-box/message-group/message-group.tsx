f03ca25bbdf974efa012d479989f176c3bd9eadb
KimiaMontazeri <kimia.montazerii@snapp.cab>
2024-01-30 13:16:06 +0330

    feat: add delete message icon and modal

--- front/src/components/chat-box/message-group/message-group.tsx
+++ front/src/components/chat-box/message-group/message-group.tsx
@@ -1,20 +1,44 @@
-import { Avatar, Flex, Stack } from '@chakra-ui/react';
+import { Avatar, Flex, IconButton, Stack } from '@chakra-ui/react';
 import TextMessage from '../text-message';
 import { MessageGroupProps } from './message-group.types';
+import { DeleteIcon } from '@chakra-ui/icons';
+import DeleteMessageModal from '../../delete-message-modal/';
+import { useState } from 'react';
 
 const MessageGroup = ({ isFromMe, from, messages }: MessageGroupProps) => {
+  const [deleteMessageModalVisibility, setDeleteMessageModalVisibility] =
+    useState(false);
+
   if (isFromMe) {
     return (
-      <Flex justifyContent='flex-end'>
-        <Flex gap={2}>
-          <Stack pt={1} alignItems='flex-end'>
-            {messages.map((text) => (
-              <TextMessage>{text}</TextMessage>
-            ))}
-          </Stack>
-          <Avatar name={from.name} src={from.photoUrl} />
+      <>
+        <DeleteMessageModal
+          isOpen={deleteMessageModalVisibility}
+          onClose={() => setDeleteMessageModalVisibility(false)}
+        />
+        <Flex justifyContent='flex-end'>
+          <Flex gap={2}>
+            <Stack pt={1} alignItems='flex-end'>
+              {messages.map((text) => (
+                <Flex>
+                  <IconButton
+                    icon={<DeleteIcon />}
+                    colorScheme='red'
+                    color='red.400'
+                    variant='ghost'
+                    isRound
+                    aria-label='delete-message'
+                    mr={1}
+                    onClick={() => setDeleteMessageModalVisibility(true)}
+                  />
+                  <TextMessage>{text}</TextMessage>
+                </Flex>
+              ))}
+            </Stack>
+            <Avatar name={from.name} src={from.photoUrl} />
+          </Flex>
         </Flex>
-      </Flex>
+      </>
     );
   }
 
