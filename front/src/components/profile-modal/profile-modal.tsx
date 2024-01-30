ce986e3ac8b656558749a5d3ea4b99d378e8f370
KimiaMontazeri <kimia.montazerii@snapp.cab>
2024-01-30 13:50:58 +0330

    refactor: improve profile picture upload in profile modal

--- front/src/components/profile-modal/profile-modal.tsx
+++ front/src/components/profile-modal/profile-modal.tsx
@@ -1,5 +1,8 @@
 import {
+  Avatar,
   Button,
+  Center,
+  Flex,
   FormControl,
   FormLabel,
   Input,
@@ -13,15 +16,56 @@ import {
   Textarea,
 } from '@chakra-ui/react';
 import type { ProfileModalProps } from './profile-modal.types';
+import { type ChangeEvent, useState } from 'react';
 
 const ProfileModal = ({ isOpen, onClose }: ProfileModalProps) => {
+  const [image, setImage] = useState<File | null>(null);
+  const [previewImage, setPreviewImage] = useState<string | null>(null);
+
+  const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const selectedImage = e.target.files?.[0];
+    if (selectedImage) {
+      setImage(selectedImage);
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setPreviewImage(reader.result as string);
+      };
+      reader.readAsDataURL(selectedImage);
+    }
+  };
+
+  const handleClearImage = () => {
+    setImage(null);
+    setPreviewImage(null);
+  };
+
+  const handleClose = () => {
+    handleClearImage();
+    onClose();
+  };
+
+  const handleSubmitProfile = () => {
+    console.log({ image });
+    // TODO: send the data to the server
+    // if ok, close the modal and show a success toast
+    // else, don't close the modal and show an error toast
+  };
+
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
+    <Modal isOpen={isOpen} onClose={handleClose}>
       <ModalOverlay />
       <ModalContent>
         <ModalHeader>Profile</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
+          <Center>
+            <Avatar
+              name='Dan Abrahmov'
+              src={previewImage ? previewImage : 'https://bit.ly/dan-abramov'}
+              size='2xl'
+              style={{ cursor: 'pointer' }}
+            />
+          </Center>
           <FormControl pb={2}>
             <FormLabel>First name</FormLabel>
             <Input
@@ -64,20 +108,27 @@ const ProfileModal = ({ isOpen, onClose }: ProfileModalProps) => {
           </FormControl>
           <FormControl pb={2}>
             <FormLabel>Profile Picture</FormLabel>
-            <Input
-              type='file'
-              //   onChange={(e) => {
-              //     const files = e.target.files;
-              //     if (files) {
-              //       const file = files[0];
-              //       setPhoto(file);
-              //     }
-              //   }}
-            />
+            <Flex gap={1}>
+              <Input
+                type='file'
+                accept='image/*'
+                onChange={handleImageChange}
+              />
+              <Button
+                disabled={true}
+                colorScheme='gray'
+                onClick={handleClearImage}
+                isDisabled={!image || !previewImage}
+              >
+                Clear
+              </Button>
+            </Flex>
           </FormControl>
         </ModalBody>
         <ModalFooter>
-          <Button variant='ghost'>Save</Button>
+          <Button variant='ghost' onClick={handleSubmitProfile}>
+            Save
+          </Button>
         </ModalFooter>
       </ModalContent>
     </Modal>
