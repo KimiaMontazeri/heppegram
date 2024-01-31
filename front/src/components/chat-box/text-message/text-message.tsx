322fd769d140d0734f8b4817630a5907277789d5
KimiaMontazeri <kimia.montazerii@snapp.cab>
2024-02-01 02:59:48 +0330

    fix: sync chat bubble with color mode

--- front/src/components/chat-box/text-message/text-message.tsx
+++ front/src/components/chat-box/text-message/text-message.tsx
@@ -4,23 +4,11 @@ import {
   Stack,
   Text,
   createIcon,
+  useColorMode,
   useColorModeValue,
 } from '@chakra-ui/react';
 import { TextMessageProps } from './text-message.types';
 import './text-message.css';
-
-const BubbleIcon = createIcon({
-  displayName: 'ChatBubbleIcon',
-  viewBox: '0 0 1 1',
-  path: (
-    <svg viewBox='0 0 1 1'>
-      <path
-        fill='#2d3748'
-        d='M1,0.897 V0 L0.912,0.21 C0.789,0.419,0.568,0.616,0.26,0.791 L0.064,0.902 C-0.021,0.951,0.095,1,0.236,0.997 L1,0.897'
-      />
-    </svg>
-  ),
-});
 
 const TextMessage = ({
   children,
@@ -28,8 +16,24 @@ const TextMessage = ({
   showBubble,
   timestamp = 12,
 }: TextMessageProps) => {
+  const { colorMode } = useColorMode();
+  const bubbleColor = colorMode === 'light' ? '#edf2f7' : '#2d3748';
   const grayColor = useColorModeValue('gray.100', 'gray.700');
   const grayTextColor = useColorModeValue('gray.500', 'gray.300');
+
+  // NOTE: we should define this outside of the component but i don't have time :))
+  const BubbleIcon = createIcon({
+    displayName: 'ChatBubbleIcon',
+    viewBox: '0 0 1 1',
+    path: (
+      <svg viewBox='0 0 1 1'>
+        <path
+          fill={bubbleColor}
+          d='M1,0.897 V0 L0.912,0.21 C0.789,0.419,0.568,0.616,0.26,0.791 L0.064,0.902 C-0.021,0.951,0.095,1,0.236,0.997 L1,0.897'
+        />
+      </svg>
+    ),
+  });
 
   if (direction === 'start') {
     return (
