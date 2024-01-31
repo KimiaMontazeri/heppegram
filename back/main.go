9df1f66d0139140d2535a87f2e32e5e5b2dd8400
Farhad Aman <farhad.aman@snapp.cab>
2024-01-31 23:52:43 +0330

    fix: cors

--- back/main.go
+++ back/main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"github.com/KimiaMontazeri/heppegram/back/db"
 	"github.com/KimiaMontazeri/heppegram/back/handlers"
-	"github.com/KimiaMontazeri/heppegram/back/middleware"
+	middle "github.com/KimiaMontazeri/heppegram/back/middleware"
 	"github.com/KimiaMontazeri/heppegram/back/repository/gorm"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"log"
 	"net/http"
 	"time"
@@ -15,6 +16,9 @@ func main() {
 	time.Sleep(5 * time.Second)
 	db.Init()
 	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.CORS())
 
 	e.GET("/", func(c echo.Context) error {
 		log.Println("Handling the request for the root route")
@@ -28,9 +32,9 @@ func main() {
 	e.POST("/api/register", userHandler.Register)
 	e.POST("/api/login", userHandler.Login)
 	e.GET("/api/users", userHandler.SearchUsers)
-	e.GET("/api/users/:username", userHandler.GetUser, middleware.JWTAuthentication)
-	e.PATCH("/api/users/:username", userHandler.UpdateUser, middleware.JWTAuthentication)
-	e.DELETE("/api/users/:username", userHandler.DeleteUser, middleware.JWTAuthentication)
+	e.GET("/api/users/:username", userHandler.GetUser, middle.JWTAuthentication)
+	e.PATCH("/api/users/:username", userHandler.UpdateUser, middle.JWTAuthentication)
+	e.DELETE("/api/users/:username", userHandler.DeleteUser, middle.JWTAuthentication)
 
 	log.Println("Starting Echo server on port 8080...")
 	e.Logger.Fatal(e.Start(":8080"))
